7ef21243065983eff2b405d6e2e03d65a70b318c
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-06 15:04:56 -0600

    Added Background and education infromation

--- client/About/Education.js
+++ client/About/Education.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Education = () => {
-    return (
-        <div>
-            <h1>Education</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Education</h2>
+      <div>
+        <img src="" alt="Fullstack Logo" />
+        <h3>Fullstack Academy of Code</h3>
+        <h4>August 2020 - October 2020</h4>
+        <p>
+          17-week Full Stack Immersive in JavaScript and basic Computer Science.
+        </p>
+      </div>
+      <div>
+        <h3>Southern Illinois University Carbondale</h3>
+        <h4>August 2010 - March 2015</h4>
+        <p>
+          Bachelor of Science in Aviation Management, Associate of Applied
+          Sciences in Aviation Flight, Minor in German, Magna Cum Laude
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Education
+export default Education;
--- client/About/index.js
+++ client/About/index.js
@@ -8,7 +8,6 @@ const About = () => {
   return (
     <div>
       <h1>Kelsey Schroeder</h1>
-      <p>I am a pilot and amateur radio operator who after taking on the challenge of updating her radio clubs website, has fallen in love with web development. Now I am a full stack developer specializing in Javascript who enjoys learning new technologies.</p>
       <Contact />
       <Background />
       <Education />
