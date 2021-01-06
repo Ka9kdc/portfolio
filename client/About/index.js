c9bd686f46b37a0cfcbae661a776545a0ecf4a68
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-06 14:41:12 -0600

    Added basic building blocks for the about me page

---
+++ client/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Background from './Background';
+import Contact from './Contact';
+import Education from './Education';
+import Hobbies from './Hobbies';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Kelsey Schroeder</h1>
+      <Contact />
+      <Background />
+      <Education />
+      <Hobbies />
+    </div>
+  );
+};
+
+export default About;
