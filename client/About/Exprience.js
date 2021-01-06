b8acd07d26d5fa62f04b21ef365d7b80bf59421b
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-06 15:37:13 -0600

    changed background section to exprience

---
+++ client/About/Exprience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exprience = () => {
+    return (
+        <div>
+            <h2>Exprience</h2>
+        </div>
+    )
+}
+
+export default Exprience
--- client/About/index.js
+++ client/About/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Background from './Background';
 import Contact from './Contact';
 import Education from './Education';
 import Hobbies from './Hobbies';
+import Exprience from './Exprience';
 
 const About = () => {
   return (
     <div>
       <h1>Kelsey Schroeder</h1>
+      <p>I am a pilot and amateur radio operator who after taking on the challenge of updating her radio clubs website, has fallen in love with web development. Now I am a full stack developer specializing in Javascript who enjoys learning new technologies.</p>
       <Contact />
-      <Background />
+      <Exprience />
       <Education />
       <Hobbies />
     </div>
