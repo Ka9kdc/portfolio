e4ee0d8ba89d0f9c70076caab62df4f1b7854ae6
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-06 14:45:57 -0600

    Basic info for contact section

--- client/About/Contact.js
+++ client/About/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Contact = () => {
     return (
         <div>
-            <h1>Contact</h1>
+            <h2>Contact</h2>
+            <div>| <Link to="github.com/Ka9kdc">Github: github.com/Ka9kdc</Link> |  | <Link to="https://www.linkedin.com/in/kelsey-m-schroeder/">Linkedin: https://www.linkedin.com/in/kelsey-m-schroeder/</Link> |</div>
         </div>
     )
 }
