2bc50abbf8e759335f55bdf53d7cc02806c66f62
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-06 15:34:41 -0600

    Added Hobbies and logos to about page

--- client/About/Education.js
+++ client/About/Education.js
@@ -5,20 +5,38 @@ const Education = () => {
     <div>
       <h2>Education</h2>
       <div>
-        <img src="" alt="Fullstack Logo" />
-        <h3>Fullstack Academy of Code</h3>
-        <h4>August 2020 - October 2020</h4>
-        <p>
-          17-week Full Stack Immersive in JavaScript and basic Computer Science.
-        </p>
+        <div>
+          <img
+            src="images/GraceHopperLongLogo.jpeg"
+            alt="Grace Hopper Program Logo"
+          />
+          <img
+            src="images/fullstack-academy-logo-color-on-white.png"
+            alt="Fullstack Logo"
+          />
+        </div>
+        <div>
+          <h3>Grace Hopper Program at Fullstack Academy of Code</h3>
+          <h4>August 2020 - October 2020</h4>
+          <p>
+            17-week Full Stack Immersive in JavaScript and basic Computer
+            Science.
+          </p>
+        </div>
       </div>
       <div>
-        <h3>Southern Illinois University Carbondale</h3>
-        <h4>August 2010 - March 2015</h4>
-        <p>
-          Bachelor of Science in Aviation Management, Associate of Applied
-          Sciences in Aviation Flight, Minor in German, Magna Cum Laude
-        </p>
+        <img
+          src="images/SIUSportLogo.png"
+          alt="Southern Illinios University Carbondale Logo"
+        />
+        <div>
+          <h3>Southern Illinois University Carbondale</h3>
+          <h4>August 2010 - March 2015</h4>
+          <p>
+            Bachelor of Science in Aviation Management, Associate of Applied
+            Sciences in Aviation Flight, Minor in German, Magna Cum Laude
+          </p>
+        </div>
       </div>
     </div>
   );
--- client/About/Hobbies.js
+++ client/About/Hobbies.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Hobbies = () => {
-    return (
-        <div>
-            <h1>Hobbies</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Hobbies</h2>
+      <ul>
+        <li>Reading</li>
+        <li>Archery</li>
+        <li>Flying</li>
+        <li>Amateur Radio</li>
+        <li>Baking</li>
+        <li>Watching Anime</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Hobbies
+export default Hobbies;
