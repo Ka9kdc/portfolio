925e4845dc28c5a63865df18735b21e5e89a731b
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-02-01 14:00:36 -0600

    Cleanup: ran prettier

--- client/About/Hobbies.js
+++ client/About/Hobbies.js
@@ -5,30 +5,39 @@ const Hobbies = (props) => {
     <div className={props.showList === 'Hobbies' ? '' : 'hidden'}>
       {/* added images */}
       <div className="hobby_planes">
-       <div className="banner_plane">
-        <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
-        <p>Reading</p></div>
-       <div className="banner_plane">
-        <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
-        <p>Archery</p></div>
-        </div><div className="hobby_planes">
-       <div className="banner_plane">
-        <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
-        <p>Flying</p></div>
-       <div className="banner_plane">
-        <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
-        <p>Amateur Radio</p></div>
-       <div className="banner_plane">
-        <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
-        <p>Baking</p></div>
-        </div><div className="hobby_planes">
-       <div className="banner_plane">
-        <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
-        <p>Watching Anime</p></div>
         <div className="banner_plane">
-        <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
-        <p>Photography</p></div>
+          <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
+          <p>Reading</p>
         </div>
+        <div className="banner_plane">
+          <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
+          <p>Archery</p>
+        </div>
+      </div>
+      <div className="hobby_planes">
+        <div className="banner_plane">
+          <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
+          <p>Flying</p>
+        </div>
+        <div className="banner_plane">
+          <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
+          <p>Amateur Radio</p>
+        </div>
+        <div className="banner_plane">
+          <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
+          <p>Baking</p>
+        </div>
+      </div>
+      <div className="hobby_planes">
+        <div className="banner_plane">
+          <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
+          <p>Watching Anime</p>
+        </div>
+        <div className="banner_plane">
+          <img src="images/Airplanes/fatRedPlanePullingBanner.png" alt="" />
+          <p>Photography</p>
+        </div>
+      </div>
     </div>
   );
 };
