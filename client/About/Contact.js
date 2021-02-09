5a8b38154da4430faaf72d34062875e6163b1fab
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-02-09 17:52:21 -0600

    contacts responsive

--- client/About/Contact.js
+++ client/About/Contact.js
@@ -3,9 +3,17 @@ import React, { useState } from 'react';
 const Contact = (props) => {
   return (
     <div className={props.showList === 'Contact' ? '' : 'hidden'}>
-      <div className="banner_plane">
-        <img src="images/using/fatRedPlanePullingBanner.png" alt="" />
-        <h2 className="big_text">Kelsey Schroeder</h2>
+      <div className="contact_planes">
+        <div className="cloud" >
+          <img src="images/using/Clouds/fumée-cartoon-png-2_no_innier_lines.png" />
+        </div>
+        <div className="banner_plane">
+          <img src="images/using/fatRedPlanePullingBanner.png" alt="" />
+          <h2 className="big_text">Kelsey Schroeder</h2>
+        </div>
+        <div className="cloud" >
+          <img src="images/using/Clouds/fumée-cartoon-png-2_no_innier_lines.png" />
+        </div>
       </div>
       <div id="contact_links">
         <a href="github.com/Ka9kdc">
