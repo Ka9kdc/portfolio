c90cdc1641994e45c4b0a5d870d320a9196eaa12
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-02-09 19:45:01 -0600

    ran prettier

--- client/About/Contact.js
+++ client/About/Contact.js
@@ -4,14 +4,14 @@ const Contact = (props) => {
   return (
     <div className={props.showList === 'Contact' ? '' : 'hidden'}>
       <div className="contact_planes">
-        <div className="cloud" >
+        <div className="cloud">
           <img src="images/using/Clouds/fumée-cartoon-png-2_no_innier_lines.png" />
         </div>
         <div className="banner_plane">
           <img src="images/using/fatRedPlanePullingBanner.png" alt="" />
           <h2 className="big_text">Kelsey Schroeder</h2>
         </div>
-        <div className="cloud" >
+        <div className="cloud">
           <img src="images/using/Clouds/fumée-cartoon-png-2_no_innier_lines.png" />
         </div>
       </div>
