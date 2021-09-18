ee60c67602c064d5e2af536f02b8991fd583514a
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-09-17 23:21:58 -0500

    Chore: move old files in to an ignored file

--- client/projects/hackathonProjectData.js
+++
@@ -1,8 +0,0 @@
-const projectsHeaders = [
-  {
-    name: 'Block Crusher',
-    deployURL: 'https://ka9kdc.github.io/BlockCrusherPuzzleGame/',
-    submissionURL: '',
-    githubURL: 'https://github.com/Ka9kdc/BlockCrusherPuzzleGame',
-  },
-];
