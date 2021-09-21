ee5f8e0c6c427fa7cf95bd4c6a9639e4a3c0bd8b
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-09-21 12:52:24 -0500

    Feat: add Education

---
+++ client/About/Education.js
@@ -0,0 +1,72 @@
+import React from "react"
+
+const Education = () => {
+    return (
+        <article>
+        <h1 className="section_title">Education</h1>
+       <div className="grid">
+        <Fullstack />
+        <Southern />
+        </div>
+        
+        </article>
+    )
+}
+
+
+const Fullstack = () => {
+   return (<section className="box">
+        <div className="grid multi_img">
+        <img
+            src="images/GraceHopperAcademyLogo.png"
+            alt="Grace Hopper Program Logo"
+          />
+<img
+            src="images/fullstack-academy-logo-color-on-white.png"
+            alt="Fullstack Logo"
+          />
+        </div>
+        <div className="title">
+          <h2 >
+            Grace Hopper Program at Fullstack Academy of Code
+          </h2>
+          <h3>Graducated: October 2020</h3>
+          </div>
+          <p>Certification in JavaScript Web Development</p>
+          <p>
+            Fullstack Academy is a top-ranked software development school based
+            in New York City. A full-time immersive program covers a broad
+            curriculum centered on full-stack JavaScript. Topics taught include
+            Node.js, React, Express, SQL databases, advanced HTML & CSS, and CS
+            fundamentals.
+          </p>
+          <p>
+            Fullstack Academy's Educational structure is project based and
+            outcome-driven, with a strong focus on teaching best practices and
+            building functional projects.
+          </p>
+    </section>)
+}
+
+const Southern = ()=> {
+ return (
+     <section className="box">
+          <div className="grid multi_img">
+          <img
+            src="images/SIUSportLogo.png"
+            alt="Southern Illinios University Carbondale Logo"
+          />
+          </div>
+          <div className="title">
+          <h2>Southern Illinois University Carbondale</h2>
+          <h3>Graduated May 2015</h3>
+          </div>
+          <p>Bachelor of Science in Aviation Management</p>
+          <p> Associate of Applied Sciences in Aviation Flight</p>
+          <p> Minor in German</p>
+          <p> Magna Cum Laude</p>
+     </section>
+ )
+}
+
+export default Education
--- client/About/index.js
+++ client/About/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Exprience from './Exprience';
 import './About.css';
+import Education from './Education';
 
 const About = () => {
   return (
     <>
-      <h1>About</h1>
-      <Exprience />
+      <h1 className="section_title">About</h1>
+      {/* <Exprience /> */}
+      <Education />
     </>
   );
 };
