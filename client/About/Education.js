fb7b4f4f6dc7c43413b158e4e9cb76baf76f066a
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-09-21 15:57:35 -0500

    Chore: Ran Prettier

--- client/About/Education.js
+++ client/About/Education.js
@@ -2,46 +2,46 @@ import React from 'react';
 
 //Add certificates to SIU
 export const EducationData = [
-    {
-      images: [
-        {
-          src: 'images/GraceHopperAcademyLogo.png',
-          alt: 'Grace Hopper Program Logo',
-        },
-        {
-          src: 'images/fullstack-academy-logo-color-on-white.png',
-          alt: 'Fullstack Logo',
-        },
-      ],
-      place: 'Grace Hopper Program at Fullstack Academy of Code',
-      graduated: 'October 2020',
-      description: [
-        'Certification in JavaScript Web Development',
-        'Fullstack Academy is a top-ranked software development school based in New York City. A full-time immersive program covers a broad curriculum centered on full-stack JavaScript. Topics taught include Node.js, React, Express, SQL databases, advanced HTML & CSS, and CS fundamentals.',
-        "Fullstack Academy's Educational structure is project based and outcome-driven, with a strong focus on teaching best practices and building functional projects.",
-      ],
-    },
-    {
-      images: [
-        {
-          src: 'images/SIUSportLogo.png',
-          alt: 'Southern Illinios University Carbondale Logo',
-        },
-        {
-          src: 'images/siuAviationPLane.png',
-          alt: 'Southern Illinios University Aviation Logo',
-        },
-      ],
-      place: 'Southern Illinois University Carbondale',
-      graduated: 'May 2015',
-      description: [
-        'Bachelor of Science in Aviation Management',
-        'Associate of Applied Sciences in Aviation Flight',
-        'Minor in German',
-        'Magna Cum Laude',
-      ],
-    },
-  ];
+  {
+    images: [
+      {
+        src: 'images/GraceHopperAcademyLogo.png',
+        alt: 'Grace Hopper Program Logo',
+      },
+      {
+        src: 'images/fullstack-academy-logo-color-on-white.png',
+        alt: 'Fullstack Logo',
+      },
+    ],
+    place: 'Grace Hopper Program at Fullstack Academy of Code',
+    graduated: 'October 2020',
+    description: [
+      'Certification in JavaScript Web Development',
+      'Fullstack Academy is a top-ranked software development school based in New York City. A full-time immersive program covers a broad curriculum centered on full-stack JavaScript. Topics taught include Node.js, React, Express, SQL databases, advanced HTML & CSS, and CS fundamentals.',
+      "Fullstack Academy's Educational structure is project based and outcome-driven, with a strong focus on teaching best practices and building functional projects.",
+    ],
+  },
+  {
+    images: [
+      {
+        src: 'images/SIUSportLogo.png',
+        alt: 'Southern Illinios University Carbondale Logo',
+      },
+      {
+        src: 'images/siuAviationPLane.png',
+        alt: 'Southern Illinios University Aviation Logo',
+      },
+    ],
+    place: 'Southern Illinois University Carbondale',
+    graduated: 'May 2015',
+    description: [
+      'Bachelor of Science in Aviation Management',
+      'Associate of Applied Sciences in Aviation Flight',
+      'Minor in German',
+      'Magna Cum Laude',
+    ],
+  },
+];
 
 const Education = () => {
   return (
@@ -77,4 +77,3 @@ export const Institute = ({ ed }) => {
 };
 
 export default Education;
-
--- client/About/Education.spec.js
+++ client/About/Education.spec.js
@@ -1,66 +1,80 @@
-import { expect } from "chai";
-import React from "react";
-import { mount, shallow } from "enzyme";
-import Education, { EducationData, Institute } from "./Education";
+import { expect } from 'chai';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Education, { EducationData, Institute } from './Education';
 
-describe("Education Component", () => {
-    let container;
-    describe("shallow render", () => {
-        before(() => {
-            container = shallow(<Education />)
-        })
-        after(() => {
-            container = null
-        })
-        it('renders an h1 cotaining experience', () => {
-            const h1 = container.find('h1');
-            expect(h1.text()).to.equal('Education');
-          });
-          it('contains Institute component', () => {
-            expect(
-              container.containsMatchingElement([<Institute key={EducationData[0].place} />])
-            ).to.equal(true);
-          });
-    })
-    describe('institute component', () => {
-        before(() => {
-            container = shallow(<Institute ed={EducationData[0]} />)
-        })
-        it("should include two logos", () => {
-            const logos = container.find("img")
-            expect(logos).to.have.lengthOf(2)
-            expect(logos.at(0).getElement().props.src).to.equal(EducationData[0].images[0].src)
-            expect(logos.at(0).getElement().props.alt).to.equal(EducationData[0].images[0].alt)
-            expect(logos.at(1).getElement().props.src).to.equal(EducationData[0].images[1].src)
-            expect(logos.at(1).getElement().props.alt).to.equal(EducationData[0].images[1].alt)
-        })
-        it("should include the institutes name", () => {
-            expect(container.contains(EducationData[0].place)).to.equal(true)
-        })
-        it("should show the year graduated", () => {
-            expect(container.contains(EducationData[0].graduated)).to.equal(true)
-            const year = container.find("h3")
-            expect(year.text()).to.be.equal(`Graduated: ${EducationData[0].graduated}`)
-        })
-        it("should contain paragraphs about the institution", () => {
-            const description = container.find("p")
-            expect(description).to.be.lengthOf(EducationData[0].description.length)
-            expect(description.at(0).text()).to.equal(EducationData[0].description[0])
-        })
-    })
-    describe('mounted render', () => {
-        before(() => {
-            container = mount(<Education />)
-        })
-        it("renders all institutions", () => {
-            const h2s = container.find("h2")
-            expect(h2s.length).to.equal(EducationData.length);
-            expect(h2s.at(0).text()).to.equal(EducationData[0].place);
-            expect(h2s.at(1).text()).to.equal(EducationData[1].place);
-        })
-        it("renders all images", () => {
-            const images = container.find("img")
-            expect(images).to.have.lengthOf(EducationData.length * 2)
-        })
-    })
-})
+describe('Education Component', () => {
+  let container;
+  describe('shallow render', () => {
+    before(() => {
+      container = shallow(<Education />);
+    });
+    after(() => {
+      container = null;
+    });
+    it('renders an h1 cotaining experience', () => {
+      const h1 = container.find('h1');
+      expect(h1.text()).to.equal('Education');
+    });
+    it('contains Institute component', () => {
+      expect(
+        container.containsMatchingElement([
+          <Institute key={EducationData[0].place} />,
+        ])
+      ).to.equal(true);
+    });
+  });
+  describe('institute component', () => {
+    before(() => {
+      container = shallow(<Institute ed={EducationData[0]} />);
+    });
+    it('should include two logos', () => {
+      const logos = container.find('img');
+      expect(logos).to.have.lengthOf(2);
+      expect(logos.at(0).getElement().props.src).to.equal(
+        EducationData[0].images[0].src
+      );
+      expect(logos.at(0).getElement().props.alt).to.equal(
+        EducationData[0].images[0].alt
+      );
+      expect(logos.at(1).getElement().props.src).to.equal(
+        EducationData[0].images[1].src
+      );
+      expect(logos.at(1).getElement().props.alt).to.equal(
+        EducationData[0].images[1].alt
+      );
+    });
+    it('should include the institutes name', () => {
+      expect(container.contains(EducationData[0].place)).to.equal(true);
+    });
+    it('should show the year graduated', () => {
+      expect(container.contains(EducationData[0].graduated)).to.equal(true);
+      const year = container.find('h3');
+      expect(year.text()).to.be.equal(
+        `Graduated: ${EducationData[0].graduated}`
+      );
+    });
+    it('should contain paragraphs about the institution', () => {
+      const description = container.find('p');
+      expect(description).to.be.lengthOf(EducationData[0].description.length);
+      expect(description.at(0).text()).to.equal(
+        EducationData[0].description[0]
+      );
+    });
+  });
+  describe('mounted render', () => {
+    before(() => {
+      container = mount(<Education />);
+    });
+    it('renders all institutions', () => {
+      const h2s = container.find('h2');
+      expect(h2s.length).to.equal(EducationData.length);
+      expect(h2s.at(0).text()).to.equal(EducationData[0].place);
+      expect(h2s.at(1).text()).to.equal(EducationData[1].place);
+    });
+    it('renders all images', () => {
+      const images = container.find('img');
+      expect(images).to.have.lengthOf(EducationData.length * 2);
+    });
+  });
+});
