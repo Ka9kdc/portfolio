c806930473a903224593b5059b81e2939a837ac5
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-10-05 22:29:57 -0500

    Fix: grammer check

--- client/About/Summary.js
+++ client/About/Summary.js
@@ -11,50 +11,50 @@ const Summary = () => {
       <article className="faded">
         <p>
           I am a pilot and amateur radio operator who after taking on the
-          challenge of updating her radio clubs website, has fallen in love with
-          web development. Now I am a full stack developer specializing in
+          challenge of updating her radio club's website, has fallen in love
+          with web development. Now I am a full-stack developer specializing in
           Javascript who enjoys learning new technologies.
         </p>
         <p>
-          My last full time job was as a Copy and Print supervisor at office
+          My last full-time job was as a Copy and Print supervisor at office
           depot. Most new Office Depot employees when you first mention the copy
-          center, they think 'oh how hard can that be, you just walk up to the
-          copier put the document in the feeder and hit start. So sure I will
-          help clients if you get a line'. Then they watch me work for an hour
-          during which I am running jobs on two copiers, printing posters, and
-          binding a book all while faxing for the customer at my counter. My job
-          at Office Depot greatly improved my ability to multitask. If I wasn't
+          center, think "oh how hard can that be, you just walk up to the copier
+          put the document in the feeder, and hit start. So sure I will help
+          clients if you get a line". Then they watch me work for an hour during
+          which I am running jobs on two copiers, printing posters, and binding
+          a book all while faxing for the customer at my counter. My job at
+          Office Depot greatly improved my ability to multitask. If I wasn't
           doing two things at once then it was a very slow day. In this
           position, I trained almost every employee in the store on how to take
           in orders, make and resize copies, laminate and fax for customers. So
           that they could back me up during peek hours. I was also in charge of
           making sure that supplies were in stock and ordered along with fixing
           broken machines or placing service calls on them. Over the years, I
-          designed posters, flyers and business cards for several customers.
+          designed posters, flyers, and business cards for several customers.
         </p>
         <p>
           Outside of work, I volunteered as the Hamfest chair for my local
-          Amateur Radio Club. A Hamfest is a radio, computer and electronic
+          Amateur Radio Club. A Hamfest is a radio, computer, and electronic
           hobby expo. In this position, I organized the club's annual Hamfest
           that typically attracted 40 vendors and 750 attendees from around the
           midwest. I handled communications between vendors and six
           subcommittees. One of the top complaints attendees and vendors had was
           that the club website was never up to date. The Hamfest committee also
           received several complaints from attendees they could not order
-          tickets online and that we were stuck in the 90's with only accepting
-          payment by mail or in person via cash or check.
+          tickets online and that we were stuck in the '90s with only accepting
+          payment by mail or in-person via cash or check.
         </p>
         <p>
           At the time no one in the club knew how to write code for a website
-          and the previous webmaster had written the site in php. So in the fall
+          and the previous webmaster had written the site in PHP. So in the fall
           of 2019, I was given access to the club website. This allowed me to
-          try to keep the Hamfest page updated myself. Over the course of the
-          next week, I taught myself to code in html and basic php and
-          Javascript. By the end of the week, I found my new passion in life and
-          had PayPal integrated into the site for attendees to place ticket
-          orders online. When I lost my job and the pandemic hit a few months
-          later. I took it as a sign to dive deeper in to my new found love of
-          coding and web development.
+          try to keep the Hamfest page updated myself. Over the next week, I
+          taught myself to code in HTML and basic PHP, and Javascript. By the
+          end of the week, I found my new passion in life and had PayPal
+          integrated into the site for attendees to place ticket orders online.
+          When I lost my job and the pandemic hit a few months later. I took it
+          as a sign to dive deeper into my newfound love of coding and web
+          development.
         </p>
       </article>
     </section>
