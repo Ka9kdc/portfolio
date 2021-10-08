caa80e0b699b8348c35fd4afeb7f955b525b0fd7
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-10-08 16:41:26 -0500

    Feat: contact form

--- client/ContactForm.js
+++ client/ContactForm.js
@@ -7,59 +7,96 @@ const ContactForm = () => {
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
   const [sent, setSent] = useState(false);
+  const [errSender, setErrSender] = useState(false);
+  const [errEmail, setErrEmail] = useState(false);
+  const [errMessage, setErrMessage] = useState(false);
 
-  const handleSubmit = () => {
-    console.log(message);
-    setSent(true);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!sender) {
+      setErrSender(true);
+    } else {
+      setErrSender(false);
+    }
+    if (!email) {
+      setErrEmail(true);
+    } else {
+      setErrEmail(false);
+    }
+    if (!message) {
+      setErrMessage(true);
+    } else {
+      setErrMessage(false);
+    }
+    if (sender && message && email) {
+      console.log(message);
+      setSent(true);
+    }
   };
 
   if (sent) {
     return (
-      <div>
+      <article>
         <h2 className="title">
           Thank you {sender} for your message. I look forward to connecting with
           you.
         </h2>
-        <ContactLinks />
-      </div>
+        <section className="box white">
+          <ContactLinks />
+        </section>
+      </article>
     );
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        className="form_input"
-        type="text"
-        placeholder="Name"
-        id="name"
-        value={sender}
-        onChange={(event) => setSender(event.target.value)}
-      />
-      <input
-        className="form_input"
-        type="email"
-        placeholder="Email"
-        id="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <input
-        className="form_input"
-        type="text"
-        placeholder="Subject"
-        id="subject"
-        value={subject}
-        onChange={(event) => setSubject(event.target.value)}
-      />
-      <textarea
-        className="form_input"
-        placeholder="Message"
-        id="Message"
-        value={message}
-        onChange={(event) => setMessage(event.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <article>
+      <form onSubmit={handleSubmit} className="box white">
+        <input
+          className="form_input"
+          type="text"
+          placeholder="Name"
+          id="name"
+          value={sender}
+          onChange={(event) => setSender(event.target.value)}
+        />
+        {errSender && <span>Please tell me your name.</span>}
+        <input
+          className="form_input"
+          type="email"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        {errEmail && <span>Please tell me how to reach you by email.</span>}
+        <input
+          className="form_input"
+          type="text"
+          placeholder="Subject"
+          id="subject"
+          value={subject}
+          onChange={(event) => setSubject(event.target.value)}
+        />
+        {errMessage && (
+          <span>
+            Please tell me why you are interested in speaking with me.
+          </span>
+        )}
+        <textarea
+          className="form_input"
+          placeholder="Message"
+          id="Message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button type="submit" className="find_out_more">
+          Submit
+        </button>
+      </form>{' '}
+      <section className="box white">
+        <ContactLinks />
+      </section>
+    </article>
   );
 };
 
