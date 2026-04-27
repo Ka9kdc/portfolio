46ab8b61241cf2fba8a863ee1ada1dc69f00e9aa
Kelsey Schroeder <67213761+Ka9kdc@users.noreply.github.com>
2026-04-27 17:25:23 -0500

    Comment out resume display and download link
    Comment out the resume iframe and download link in Resume.js.

--- src/About/Resume.js
+++ src/About/Resume.js
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom';
 
 const Resume = () => (
   <>
-    <div className="video_wrapper">
-      <iframe
-        id="Resume"
-        className="responsive-iframe"
-        src="Kelsey_Schroeder_Resume_September_2021.pdf"
-      />
-    </div>
-    <Link
-      to="Kelsey_Schroeder_Resume_September_2021.pdf"
-      target="_blank"
-      download
-      className="find_out_more_container "
-    >
-      <button type="button" className="title find_out_more">
-        Download My Resume
-      </button>
-    </Link>
+    // <div className="video_wrapper">
+    //   <iframe
+    //     id="Resume"
+    //     className="responsive-iframe"
+    //     src="Kelsey_Schroeder_Resume_September_2021.pdf"
+    //   />
+    // </div>
+    // <Link
+    //   to="Kelsey_Schroeder_Resume_September_2021.pdf"
+    //   target="_blank"
+    //   download
+    //   className="find_out_more_container "
+    // >
+    //   <button type="button" className="title find_out_more">
+    //     Download My Resume
+    //   </button>
+    // </Link>
   </>
 );
 
